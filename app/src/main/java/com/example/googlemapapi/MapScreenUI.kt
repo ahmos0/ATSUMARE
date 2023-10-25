2ea52f7e9dde9e3bcfdbfebcfd21eaf8b45deeea
Hina524 <hina15520@gmail.com>
2023-10-25 15:08:36 +0900

    赤ボタン＆アイコンのサイズ＆位置調整

--- app/src/main/java/com/example/googlemapapi/MapScreenUI.kt
+++ app/src/main/java/com/example/googlemapapi/MapScreenUI.kt
@@ -1,11 +1,8 @@
 package com.example.googlemapapi
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,45 +12,23 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.colorspace.WhitePoint
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.model.LatLng
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,55 +38,98 @@ fun TopAppBarSample() {
         title = {
             Text(
                 text = "ATUMARE",
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth() .offset(x = (-10).dp),
                 textAlign = TextAlign.Center
             )
         }
     )
 }
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomBar(navController: NavController,modifier: Modifier = Modifier) {
+fun BottomBar(navController: NavController, modifier: Modifier = Modifier) {
     val carImage: Painter = painterResource(R.drawable.directions_car_24px)
     val peopleImage: Painter = painterResource(R.drawable.emoji_people_24px)
-    val ImageSizeModifier = Modifier
-        .size(70.dp)
-    //val ButtonPositionModifier = Modifier.offset(y = 0.dp)
+    val buttonImage: Painter = painterResource(R.drawable.radio_button_checked_24px)
+    val ImageSizeModifier = Modifier.size(70.dp)
 
     Scaffold(
         bottomBar = {
-            BottomAppBar(
-                modifier = modifier
-                    .height(72.dp)
-                    .fillMaxWidth(),
-                containerColor = Color.White,
-                contentPadding = PaddingValues(start = 16.dp, end = 16.dp)
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 40.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    IconButton(onClick = {navController.navigate("anotherScreen") }) {
-                        Icon(carImage, contentDescription = null, modifier = ImageSizeModifier)
-                    }
-                    FloatingActionButton(onClick = { /*TODO*/ },
-                        shape = CircleShape,
-                        containerColor = Color(204,102,119)){
-                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
-                    }
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            peopleImage,
-                            contentDescription = "Localized description",
-                            modifier = ImageSizeModifier,
-                        )
-                    }
-                }
+            Box {
+                BottomBarContent(
+                    carImage = carImage,
+                    peopleImage = peopleImage,
+                    ImageSizeModifier = ImageSizeModifier,
+                    navController = navController
+                )
+                FloatingActionOverlayButton(
+                    buttonImage = buttonImage,
+                )
             }
-        },
+        }
     ) { innerPadding ->
         GoogleMapScreen(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BottomBarContent(
+    carImage: Painter,
+    peopleImage: Painter,
+    ImageSizeModifier: Modifier,
+    navController: NavController
+) {
+    BottomAppBar(
+        modifier = Modifier
+            .height(72.dp)
+            .fillMaxWidth()
+            .offset(y = 623.dp),
+        containerColor = Color.White,
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .offset(x = 10.dp), //アイコン二つを右にずらす
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                IconButton(onClick = { navController.navigate("anotherScreen") }) {
+                    Icon(carImage, contentDescription = null, modifier = ImageSizeModifier)
+                }
+                Text(text = "ATUMERU", modifier = Modifier .offset(x = -7.dp))
+            }
+            Column {
+                IconButton(onClick = { navController.navigate("JoinScreen") }) {
+                    Icon(
+                        peopleImage,
+                        contentDescription = "Localized description",
+                        modifier = ImageSizeModifier
+                    )
+                }
+                Text(text = "ATUMARU", modifier = Modifier .offset(x = -7.dp))
+            }
+        }
+    }
+}
+@Composable
+fun FloatingActionOverlayButton(
+    buttonImage: Painter,
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(), // 必要に応じて修正してください
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .offset(y = 275.dp, x = (-157).dp)
+                .size(75.dp),
+            shape = CircleShape,
+            containerColor = Color(228, 98, 102)
+        ) {
+            Icon(buttonImage, contentDescription = null, Modifier.size(45.dp))
+        }
     }
 }
