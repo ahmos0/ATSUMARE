addce1be9c8e7a15098b50779bf230e658e7e504
Hina524 <hina15520@gmail.com>
2023-10-30 18:17:04 +0900

    一旦でけた

--- app/src/main/java/com/example/googlemapapi/MainActivity.kt
+++ app/src/main/java/com/example/googlemapapi/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.googlemapapi
 
-import android.location.Location
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
@@ -15,17 +17,9 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.android.volley.BuildConfig
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.libraries.places.api.Places
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/googlemapapi/PassengerRequestScreen.kt
@@ -0,0 +1,86 @@
+package com.example.googlemapapi
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun PssengerRequestScreenUI() {
+    var name by remember { mutableStateOf(TextFieldValue()) }
+    var request by remember { mutableStateOf(TextFieldValue()) }
+    Box {
+        GoogleMapScreen()
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            val width = size.width
+            val height = size.height
+            drawRoundRect(
+                color = Color(230, 230, 230),
+                topLeft = Offset(60f, 1900f),
+                size = Size(width - 2 * 60f, height - 2 * 1200f),
+                cornerRadius = CornerRadius(x = 25F, y = 25F)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .offset(y = 250.dp)
+                .fillMaxSize()
+                .padding(35.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.height(2.dp))
+            TextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("名前") },
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(45.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                ),
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            TextField(
+                value = request,
+                onValueChange = { request = it },
+                label = { Text("要望をここに入力") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(45.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                ),
+            )
+        }
+    }
+}
